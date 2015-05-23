b782dab95131de919317d28e1daeddd22bbc36d8
Danilo Oliveira <daniloa.oliveira@gmail.com>
2015-05-22 22:03:24 -0300

    parte 1
    http://twilioinc.wpengine.com/2012/09/building-a-real-time-sms-voting-ap
    p-part-1-node-js-couchdb.html

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+/**
+ * Module dependencies.
+ */
+
+
+
+
 /*jshint node:true*/
 
 //------------------------------------------------------------------------------
@@ -6,7 +13,10 @@
 
 // This application uses express as it's web server
 // for more info, see: http://expressjs.com
-var express = require('express');
+express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
@@ -16,14 +26,36 @@ var cfenv = require('cfenv');
 var app = express();
 
 // serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'hjs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded());
+  app.use(express.methodOverride());
+  app.use(express.urlencoded());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', routes.index);
+app.post('/vote/sms', routes.voteSMS);
+
+
 // start server on the specified port and binding host
 app.listen(appEnv.port, appEnv.bind, function() {
 
-	// print a message when the server starts listening
+  // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
